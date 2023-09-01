7d4740baa45ed9ee2b5ed3160db489c0124b4a42
Lorcan Nicholls <ln356@cam.ac.uk>
2023-09-01 11:52:38 +0100

    minor changes

--- src/collect_data_6_fingers.py
+++ src/collect_data_6_fingers.py
@@ -5,9 +5,9 @@ import numpy as np
 def main():
     # setup equipment
     ross = RectEIT()
-    ross._OUTPUT_FILE = 'output/EIT_Data_Gelatin_6_fingers - SD.xlsx'
+    ross._OUTPUT_FILE = 'output/EIT_Data_Gelatin_6_fingers - SD - demo set.xlsx'
     ross.connect_to_hardware(move=True, confirm=True)
-    ross.test_six_finger_motor(num_times=2)
+    ross.test_six_finger_motor(num_times=3)
     ross.get_random_six_finger_data(num_trials=1000)
 
 
